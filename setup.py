fa3a8dc001e8ee44d93a69581bea2a88b7191ea3
mjadiaz <jmauricio.adiaz@gmail.com>
2022-07-26 01:58:22 +0100

    first commit

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='toy_models',
+    version='0.0.1',
+    install_requires=[
+        'gym',
+        'matplotlib',
+        'jupyterthemes',
+        'scikit-learn']
+)
---
+++ toy_models/__init__.py
@@ -0,0 +1,6 @@
+from gym.envs.registration import register
+
+register(
+    id='ToyFunction2d-v1',
+    entry_point='toy_models.envs:ToyFunction2d_v1',
+)
