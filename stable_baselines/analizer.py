30ad28fd06862a3a0597147ebe6f066eb102653b
mjadiaz <jmauricio.adiaz@gmail.com>
2022-11-23 11:53:02 +0000

    vis update, using callbacks

--- stable_baselines/analizer.py
+++ stable_baselines/analizer.py
@@ -12,6 +12,7 @@ from PIL import Image, ImageDraw
 from train_sb3 import alg_name, env_config, env
 from stable_baselines3 import DDPG
 import pathlib
+from toy_models.envs.utils import minmax
 
 def fig2img(fig):
     """Convert a Matplotlib figure to a PIL Image and return it"""
@@ -42,13 +43,19 @@ critic = model.critic
 actor = model.policy
 
 
-tab1, tab2, tab3, tab4 = st.tabs(["Q-Network", "Policy", 'Combined sampling', 'QA Sampling'])
+tab1, tab2, tab3, tab4 = st.tabs([
+    "Q-Network", 
+    "Policy", 
+    'Combined sampling', 
+    'QA Sampling'])
 
-
-with tab1:
-    samples = 100000
+def q_values_one_state(critic, n_points, state=None):
+    samples = n_points 
     action_space = np.random.uniform(-1,1,size=(samples, 2))
-    state_space = np.repeat(np.random.uniform(-1,1,size=(1,2)), samples, axis=0)
+    if state is None: 
+        state_space = np.repeat(np.random.uniform(-1,1,size=(1,2)), samples, axis=0)
+    else:
+        state_space = np.repeat(state.reshape((1,2)), samples, axis=0)
     state = state_space[0]
     next_states =  action_space
     action_space = torch.tensor(action_space).float()
@@ -56,7 +63,74 @@ with tab1:
 
     q_values = critic(state_space, action_space)
     q_values = q_values[0].flatten().detach().numpy()
+    return state, action_space.detach().numpy(), q_values.reshape((n_points, 1))
 
+def q_values_n_best(state, action_space, q_values, n_best=5):
+    state_space = np.repeat(state.reshape((1,2)), len(q_values), axis=0)
+    # Merge into one matrix
+    actions_q_values = np.hstack([action_space, q_values])
+    # Sort by q_values
+    actions_q_values = actions_q_values[actions_q_values[:,-1].argsort()] 
+    # Desending order and select first n_best
+    actions_q_values = np.flip(actions_q_values, axis=0)[:n_best]
+    # Define index
+    actions_index = np.arange(len(actions_q_values)).reshape(-1,1)
+    # Get Soft Max for q values
+    soft_max_q = softmax(actions_q_values[:,-1])
+    # Separate in individuals arrays
+    actions = actions_q_values[:, :2]
+    q_values = actions_q_values[:, -1]
+    return  state, actions, q_values, soft_max_q, actions_index
+
+def mc_total_integration( n_points=100000, dim=4):
+    a,b = -1,1
+    action_space = np.random.uniform(a,b,size=(n_points,2)) 
+    state_space = np.random.uniform(a,b,size=(n_points,2))
+
+    q_values = critic(
+            torch.tensor(state_space).float(),
+            torch.tensor(action_space).float()
+            )
+
+    q_values = q_values[0].flatten().detach().numpy()
+
+    soft_max_q = softmax(q_values)
+    norm_data = minmax(
+            q_values, 
+            [q_values.min(), q_values.max()],
+            [0,1])
+    
+    y_mean = np.sum(norm_data)/n_points
+    integ = ((b-a)**dim)*y_mean
+
+    return integ
+
+with tab3:
+    st.write(mc_total_integration(n_points=100))
+    integration = []
+    x = np.logspace(2, 5, num=100)
+    x = np.array([int(i) for i in x])
+    for i in x:
+        integ = mc_total_integration(n_points=i)
+        integration.append(integ)
+    fig, ax = plt.subplots()
+    ax.plot(x,integration)
+    st.pyplot(fig)
+
+with tab4:
+    n_points, n_best = 100, 10
+    state, action_space, q_values = q_values_one_state(critic, n_points)
+    state, actions, q_values, soft_max_q, actions_index =\
+            q_values_n_best(state, action_space, q_values, n_best)
+    st.write(soft_max_q)
+    state, actions, q_values = q_values_one_state(critic, 1)
+    st.write(q_values_one_state(critic,1))
+
+
+with tab1:
+
+    samples = 100000
+    state, next_states, q_values = q_values_one_state(critic, samples)
     fig, ax = plt.subplots(figsize = (4,4))
     
     ax.title.set_text('Critic (Q network)')
--- stable_baselines/train_sb3.py
+++ stable_baselines/train_sb3.py
@@ -6,9 +6,12 @@ from stable_baselines3 import TD3
 
 from toy_models.envs.toy_functions import TF2D_DEFAULT_CONFIG
 from stable_baselines3.common.noise import NormalActionNoise, OrnsteinUhlenbeckActionNoise
+from stable_baselines3.common.callbacks import CheckpointCallback, CallbackList
+from stable_baselines3.common.callbacks import EveryNTimesteps, EvalCallback
+from stable_baselines3.common.callbacks import BaseCallback 
+from stable_baselines3.common.logger import TensorBoardOutputFormat
 
-
-alg_name = 'TD3_double_gaussian_2'
+alg_name = 'pendulum'
 
 env_config = TF2D_DEFAULT_CONFIG
 env_config.kernel_bandwidth = 0.05
@@ -17,22 +20,54 @@ env_config.norm_min = -1
 env_config.norm_max = 1
 env_config.function_name = "double_gaussian"
 
+def make_env(env): 
+    """
+    See https://colab.research.google.com/github/Stable-Baselines-Team/rl-colab-notebooks/blob/sb3/multiprocessing_rl.ipynb
+    for more details on vectorized environments
 
-env = gym.make("ToyFunction2d-v1")
-#env = gym.make('Pendulum-v1')
-if __name__ == '__main__':
+    Utility function for multiprocessed env.
     
+    :param env_id: (str) the environment ID
+    :param num_env: (int) the number of environment you wish to have in subprocesses
+    :param seed: (int) the inital seed for RNG
+    :param rank: (int) index of the subprocess
+    :return: (Callable)
+    """
+    def _init():
+        return env
+    return _init
+
+
+#env = gym.make("ToyFunction2d-v1")
+env_id = 'Pendulum-v1'
+env = gym.make(env_id)
+if __name__ == '__main__':
+    # Define Callbacks 
+    # this is equivalent to defining CheckpointCallback(save_freq=500)
+    # checkpoint_callback will be triggered every 500 steps
+    #checkpoint_on_event = CheckpointCallback(
+    #        save_freq=1, 
+    #        save_path='./logs/'+alg_name
+    #        )
+    #eval_callback_on_event = EvalCallback(
+    #        env, 
+    #        best_model_save_path='./logs/'+alg_name+'/best',
+    #        log_path='./logs/'+alg_name, 
+    #        eval_freq = 1
+    #        )
+    #callbacks = CallbackList([checkpoint_on_event, eval_callback_on_event])
+    #callbacks = EveryNTimesteps(n_steps=500, callback=callbacks)
     # The noise objects for DDPG
     n_actions = env.action_space.shape[-1]
-    action_noise = NormalActionNoise(mean=np.zeros(n_actions), sigma=0.4* np.ones(n_actions))
+    action_noise = NormalActionNoise(mean=np.zeros(n_actions), sigma=0.3* np.ones(n_actions))
     
-    model = TD3("MlpPolicy", env, action_noise=action_noise, verbose=1,tensorboard_log="./"+alg_name+"_toy_tensorboard/")
+    model = TD3("MlpPolicy", env, action_noise=action_noise, verbose=1,tensorboard_log="./logs/"+alg_name)
     
-    model.learn(total_timesteps=120_000)
+    model.learn(total_timesteps=120_000, callback=callbacks)
     
     # Saving the final model
     #model.save("models/sac")
-    model.save("test/"+alg_name)
+    model.save("./logs/"+alg_name+'/final')
     
     obs = env.reset()
     for i in range(500):
