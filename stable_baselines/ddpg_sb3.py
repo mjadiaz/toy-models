e40614e877c5b53a8692d3360699c40bd73f2fad
mjadiaz <jmauricio.adiaz@gmail.com>
2022-11-23 12:14:41 +0000

    env runing with td3 positive rewards found

--- stable_baselines/ddpg_sb3.py
+++ stable_baselines/ddpg_sb3.py
@@ -6,32 +6,41 @@ from stable_baselines3 import DDPG, TD3
 from bsm_models.envs.bsm_environment import PHENOENV_DEFAULT_CONFIG
 from stable_baselines3.common.noise import NormalActionNoise, OrnsteinUhlenbeckActionNoise
 
-from stable_baselines3.common.callbacks import CheckpointCallback
+from stable_baselines3.common.callbacks import CheckpointCallback, CallbackList, EvalCallback
 
 
 from bsm_checker import hep_config
 
-alg_name = 'TD3'
+alg_name = 'blssm_td3_1'
 
 env_config =  PHENOENV_DEFAULT_CONFIG
-env_config.kernel_bandwidth = 0.05
+env_config.kernel_bandwidth = 0.1
 env_config.parameter_shift_mode = False
 env_config.norm_min = -1
 env_config.norm_max = 1
 
 env = gym.make("PhenoEnv-v3", env_config=env_config, hep_config=hep_config)
+
+eval_env = gym.make("PhenoEnv-v3", env_config=env_config, hep_config=hep_config)
+
 #env = gym.make('Pendulum-v1')
 
 # The noise objects for DDPG
 n_actions = env.action_space.shape[0]
 action_noise = NormalActionNoise(mean=np.zeros(n_actions), sigma=0.25 * np.ones(n_actions))
 
-model = TD3("MlpPolicy", env, action_noise=action_noise, verbose=1,tensorboard_log="./"+alg_name+"_bsm_tensorboard/")
+model = TD3("MlpPolicy", env, action_noise=action_noise, verbose=1,tensorboard_log="./logs/"+alg_name)
 
 # Save a checkpoint every 1000 steps
-checkpoint_callback = CheckpointCallback(save_freq=500, save_path='./logs/',
-                                         name_prefix='rl_model')
-model.learn(total_timesteps=100_000,callback=checkpoint_callback)
+checkpoint_callback = CheckpointCallback(save_freq=500, save_path='./logs/'+alg_name+'/chckpts/',
+                                         name_prefix='model')
+
+# Separate evaluation env
+eval_callback = EvalCallback(eval_env, best_model_save_path='./logs/'+alg_name+'/results/best_model',
+                             log_path='./logs/'+alg_name+'/results', eval_freq=100)
+# Create the callback list
+callback = CallbackList([checkpoint_callback, eval_callback])
+model.learn(total_timesteps=10_000,callback=callback)
 
 # Saving the final model
 #model.save("models/sac")
--- stable_baselines/submit.sh
+++ stable_baselines/submit.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 #SBATCH --job-name=trainer
-#SBATCH --time=16:00:00
+#SBATCH --time=24:00:00
 #SBATCH --nodes=1
 #SBATCH --tasks-per-node=1
-#SBATCH --cpus-per-task=30
+#SBATCH --cpus-per-task=10
 
 source /home/mjad1g20/.bashrc
 source activate phenogame
